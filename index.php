dd7fef7c18b0b4579aed9d199fb9c63437953dd0
rus-ik <igorek_k@hotmail.com>
2019-06-19 22:26:56 +1000

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ function extract_sms_data ($sms_text) {
 	if (preg_match_all($regex_for_numbers, $sms_text, $matches) && isset($matches[0])) {
 		foreach ($matches[0] as $k => $number)	 {
 			if ((preg_match('/\d{11,20}/', $number) && !$wallet && ($wallet = $number)) ||
-				(preg_match('/'.$amount_suffixes.'$/', $number) && !$code && ($code = $number))
+				(preg_match('/[^('.$amount_suffixes.'|(\.|,)\d{0,2})]$/', $number) && !$code && ($code = $number))
 			) {
 				unset($matches[0][$k]);
 			}
